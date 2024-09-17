cace78c4d30220428360ef4d6eb80816c2a6b44a
asrithasri <asrithk133@gmail.com>
2024-09-17 17:55:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import './App.css';
+
+
+
+const dictionaryData = 
+  [
+  
+      { word: "React", meaning: "A JavaScript library for building user interfaces." },
+  
+      { word: "Component", meaning: "A reusable building block in React." },
+  
+      { word: "State", meaning: "An object that stores data for a component." }
+  
+  ];
+
+function App() {
+
+  
+  const [searchTerm, setSearchTerm] = useState("");
+  const [definition ,setDefinition] = useState("");
+
+  const handleSearch =()=>{
+
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    const wordData = dictionaryData.find(
+      (entry)=>entry.word.toLowerCase()=== lowerCaseSearchTerm);
+
+      if(wordData){
+        setDefinition(wordData.meaning)
+      }
+      else{
+        setDefinition("Word not found in the dictionary");
+      }
+
+  };
+
+
+  return (
+    <div className="App">
+      <h1>Dictionary App</h1>
+      <br></br>
+      <input type ="text" value={searchTerm}
+      onChange={(e)=>setSearchTerm(e.target.value)}
+      placeholder='search here'>
+      </input>
+      <button  onClick={handleSearch}>Search</button>
+
+      <br></br>
+      <h4>Definition:</h4>
+      {definition && <p>{definition}</p>}
+    </div>
+  );
+}
+
+export default App;
